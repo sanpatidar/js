c5359891e70773d415ff704fe391b451e4a88d30
Sanskar Patidar <149409060+sanpatidar@users.noreply.github.com>
2025-01-30 13:24:38 +0000

    datatypes

---
+++ .devcontainer/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3 + 3)
+console.log("Sanskar")
+
+let name = "Sanskar"
+let age = 18;
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefine =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof null); //object
+console.log(typeof undefine); //undefine
